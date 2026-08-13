127751f6c191efc5bd607ceb64954bd7c1c0c11a
holmesb <5072156+holmesb@users.noreply.github.com>
2026-08-13 22:56:58 +0100

    Fixes #24. Attach schemautil.IsMAC() + schemautil.NormalizeMAC() to remaining resources that use a mac address attribute.

--- internal/provider/resource_interface_macvlan.go
+++ internal/provider/resource_interface_macvlan.go
@@ -11,9 +11,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	"github.com/ebogdum/terraform-provider-routeros/internal/client"
+	"github.com/ebogdum/terraform-provider-routeros/internal/schemautil"
 )
 
 var (
@@ -109,9 +111,11 @@ func (r *InterfaceMacvlanResource) Schema(_ context.Context, _ resource.SchemaRe
 				Description: "Whether the entry is disabled.",
 			},
 			"mac_address": schema.StringAttribute{
-				Optional:    true,
-				Computed:    true,
-				Description: "",
+				Optional:      true,
+				Computed:      true,
+				Description:   "",
+				Validators:    []validator.String{schemautil.IsMAC()},
+				PlanModifiers: []planmodifier.String{schemautil.NormalizeMAC()},
 			},
 			"mode": schema.StringAttribute{
 				Optional:    true,
